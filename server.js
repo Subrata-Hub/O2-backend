1ea268c0f00762cd3f716c221cfb6ed697102711
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-09-09 20:00:28 +0530

    final

---
+++ server.js
@@ -0,0 +1,35 @@
+// import payload from "payload"
+// import config from "./payload.config"
+
+// const start = async () => {
+//   await payload.init({
+//     config, // use imported config directly
+//     onInit: () => {
+//       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+//     },
+//   })
+// }
+
+// start()
+
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`,
+  )
+})
